6d5614c405e147d33260c99af43b40b3637324b0
Haoran Peng (彭浩然) <43915084+Haoran-Peng@users.noreply.github.com>
2022-11-06 18:28:09 +0800

    Add files via upload

---
+++ TD3-SingleUT-Two/TD3.py
@@ -0,0 +1,33 @@
+import gym
+import gym_foo
+import numpy as np
+import math
+from stable_baselines3 import TD3
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+env = gym.make('foo-v0')
+
+n_actions = env.action_space.shape[-1]
+
+model = TD3("MlpPolicy", env, action_noise=None, learning_rate=0.00001, buffer_size=pow(2, 22), batch_size=pow(2, 12), verbose=1, tensorboard_log="./TD3_SingleUT_Two_tensorboard/")
+model.learn(total_timesteps=3240000, log_interval=10)
+model.save("td3_SingleUT_Two")
+env = model.get_env()
+
+del model # remove to demonstrate saving and loading
+
+model = TD3.load("td3_SingleUT_Two")
+
+obs = env.reset()
+R = 0
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    R += rewards
+    if dones==True:
+        break
+        
+    env.render()
+print(R)
